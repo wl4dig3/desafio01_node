777a47e11db6989b75ded7cfc75bac5484643388
Wladimir Guevara Berbesi <wladige@gmail.com>
2024-06-14 21:20:45 -0400

    feat: se finaliza

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+
 const { registrar, leer } = require('./operaciones');
 
+const [operacion, nombre, edad, tipo, color, enfermedad] = process.argv.slice(2); 
+// const argumegntos = process.argv;
+// console.log('ARGUMENTIS', argumentos);
+// console.log('que es?', stringify(registrar));
+
+if (operacion === 'registrar') {
+    registrar(nombre, edad, tipo, color, enfermedad);
+} else if (operacion === 'leer') {
+    leer();
+} else {
+    console.log('Esta vaina no se encuentra');
+}
--- operaciones.js
+++ operaciones.js
@@ -1,12 +1,32 @@
-const registrar = (nombreAnimal, Edad, Tipo, color, enfermedad) => {
+const fs = require('fs');
+
+const registrar = (nombre, edad, tipo, color, enfermedad) => { 
+    const registros = JSON.parse(fs.readFileSync('./citas.json', 'utf-8')); 
+    const nuevoRegistro = {
+        nombre,
+        edad,
+        tipo,
+        color,
+        enfermedad
+    }
+
+    registros.push(nuevoRegistro)
+    fs.writeFileSync('./citas.json', JSON.stringify(registros))
     
 };
 
 const leer = () => {
   //mostrar por consola las citas de animales registrados  
+  const registros = JSON.parse(fs.readFileSync('./citas.json', 'utf-8'))
+  console.log(registros)
+
+  const registroJSON = JSON.parse(registros)
+  for(const registro of registroJSON) {
+    console.log(registro)
+  }
 };
 
-module.export = {
+module.exports = {
   registrar,
   leer
 }; 
