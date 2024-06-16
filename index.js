9f3feedf8a3221044fdc7407e00c7a01c9a3ddf0
Wladimir Guevara Berbesi <wladige@gmail.com>
2024-06-16 16:49:27 -0400

    chore: se borra lineas comentadas

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 const { registrar, leer } = require('./operaciones');
 
 const [operacion, nombre, edad, tipo, color, enfermedad] = process.argv.slice(2); 
-// const argumegntos = process.argv;
-// console.log('ARGUMENTIS', argumentos);
-// console.log('que es?', stringify(registrar));
 
 if (operacion === 'registrar') {
     registrar(nombre, edad, tipo, color, enfermedad);
